c677e2a8ebce3dfa4d9f29f2611922298937f969
Souvik-Halder <souvik.halder2020@gift.edu.in>
2025-04-04 22:18:08 +0530

    focus issue fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,24 +9,36 @@ import {
   ViewChildren,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements AfterViewInit {
   @ViewChild('myInput') myInput: any;
 
+  constructor(private router: Router) {}
+
   ngAfterViewInit() {
     // Use setTimeout to ensure the input is fully rendered before focusing
     setTimeout(() => {
       if (this.myInput) {
         this.myInput.nativeElement.focus();
+        this.myInput.nativeElement.autoFocus = true;
+        this.router.events
+          .pipe(filter((event) => event instanceof NavigationEnd))
+          .subscribe(() => {
+            if (this.myInput) {
+              this.myInput.nativeElement.focus();
+            }
+          });
       }
+      this.myInput.native;
     }, 300); // Adding a small delay to ensure it works properly
   }
 }
